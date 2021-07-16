4e1c7d2f954c4a3fe10c6cb8346be36c39ed697c
Ben Gregory <JBGregoryPro@protonmail.com>
2021-07-16 15:39:01 -0500

    added input function for adding new toys with ID

--- leonidstoys.js
+++ leonidstoys.js
@@ -1,26 +1,24 @@
-const toyToFind = 6
+// const toyToFind = 6
 
 const toys = [
 {
     id: 1,
-    name: "Bop It",
+    name: "Lil' Docs Liposuction Playset",
     maker: "Hasbro",
     price: 29.99,
     cost: 20,
     ageGroup: "10+",
     inventory: 3
 },
-
- {
+{
     id: 2,
-    name: "Slingshot",
-    maker: "Mr Slingshot",
+    name: "Baby's First TIG Welder",
+    maker: "Joanne Fabrics",
     price: 25.99,
     cost: 15,
     ageGroup:  "12",
     inventory: 5
 },
-
 {
     id: 3,
     name: "Mecha Nixon Action Figure",
@@ -28,13 +26,12 @@ const toys = [
     price: 54.99,
     cost: 40,
     ageGroup: "18+",
-     inventory: 4
+    inventory: 4
 }
 ]
-
 const lawnDarts = {
     
-    id: 5,
+    
     name: "Lawn Dart Battle Set",
     maker: "ACME",
     price: 34.99,
@@ -42,10 +39,10 @@ const lawnDarts = {
     ageGroup: "All Ages",
     inventory: 6
 }
-toys.push(lawnDarts)
+// toys.push(lawnDarts)
 
 const flameThrower = {
-        id: 6,
+        
         name: "Fun Time Flamethrower",
         maker: "Ikea",
         price: 99.99,
@@ -54,13 +51,46 @@ const flameThrower = {
         inventory: 9
     }
 
-    toys.push(flameThrower)
+    const elmo = {
+        
+        name: "Lemme Bum a Smoke Elmo",
+        maker: "Mattel",
+        price: 59.99,
+        cost: 32,
+        ageGroup: "7+",
+        inventory: 13
+    }    
 
-for (const toy of toys) {
-    if (toy.id === toyToFind) {
-        toy.price = toy.price * 1.05
-        console.log(`The ${toy.name} is made by ${toy.maker} and costs $${toy.price}  
-`);
+    const throwingStars = {
+        name: "Ninja Throwing Stars",
+        maker: "Pediatric Eye Surgeons Inc",
+        price: 14.99,
+        cost: 5,
+        ageGroup: "10+",
+        inventory: 47
     }
+
+   
+
+const addToyToInventory = (toyObject) => {
+    const lastIndex = toys.length -1
+    const currentLastToy = toys[lastIndex]
+    const maxId = currentLastToy.id
+    const idForNewToy = maxId + 1
+    toyObject.id = idForNewToy
+    toys.push(toyObject)
 }
 
+
+addToyToInventory(flameThrower)
+addToyToInventory(throwingStars)
+addToyToInventory(elmo)
+addToyToInventory(lawnDarts)
+
+
+for (const toy of toys) {
+    // if (toy.id === toyToFind) {
+    //     toy.price = toy.price * 1.05
+        console.log(`The ${toy.name} is made by ${toy.maker} and costs $${toy.price} and its ID is ${toy.id} 
+`);
+    }
